6f5a1fc89b11036f22a9ee41ca8cac7325ace86d
loussalmohammed <hplaptopsimo@gmail.com>
2024-11-13 16:17:13 +0100

    add Base controller, for generic controller

---
+++ src/main/java/org/wrmList/waitingRoom/common/controller/BaseController.java
@@ -0,0 +1,53 @@
+package org.wrmList.waitingRoom.common.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.wrmList.waitingRoom.common.service.BaseService;
+import org.wrmList.waitingRoom.shared.dto.PageDTO;
+import org.wrmList.waitingRoom.util.annotation.IdExists;
+
+@RestController
+public abstract class BaseController<ID, CreateDTO, UpdateDTO, ResponseDTO> {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    protected abstract BaseService<ID, CreateDTO, UpdateDTO, ResponseDTO> getService();
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDTO create(@RequestBody CreateDTO createDTO) {
+        logger.info("Creating entity with data: {}", createDTO);
+        return getService().create(createDTO);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO findById(@IdExists(entityClass = Object.class) @PathVariable ID id) {
+        logger.info("Finding entity by ID: {}", id);
+        return getService().findById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public PageDTO<ResponseDTO> findAll(Pageable pageable) {
+        logger.info("Retrieving all entities with pagination");
+        return getService().findAll(pageable);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO updateById(@RequestBody UpdateDTO updateDTO, @IdExists(entityClass = Object.class) @PathVariable ID id) {
+        logger.info("Updating entity with ID: {}", id);
+        return getService().updateById(updateDTO, id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@IdExists(entityClass = Object.class) @PathVariable ID id) {
+        logger.info("Deleting entity with ID: {}", id);
+        getService().deleteById(id);
+    }
+}
