0128496b0d818e7f5617800f7a4a271cc3563eb2
loussalmohammed <hplaptopsimo@gmail.com>
2024-11-15 11:17:29 +0100

    add visitor controller

---
+++ src/main/java/org/wrmList/waitingList/visitor/controller/VisitorController.java
@@ -0,0 +1,29 @@
+package org.wrmList.waitingList.visitor.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wrmList.waitingList.common.controller.BaseController;
+import org.wrmList.waitingList.common.service.BaseService;
+import org.wrmList.waitingList.visitor.dto.CreateVisitorDTO;
+import org.wrmList.waitingList.visitor.dto.ResponseVisitorDTO;
+import org.wrmList.waitingList.visitor.dto.UpdateVisitorDTO;
+import org.wrmList.waitingList.visitor.entity.Visitor;
+import org.wrmList.waitingList.visitor.service.VisitorService;
+
+@RestController
+@RequestMapping("/visitor")
+@RequiredArgsConstructor
+public class VisitorController extends BaseController<Visitor, Long, CreateVisitorDTO, UpdateVisitorDTO, ResponseVisitorDTO> {
+    private final VisitorService visitorService;
+
+    @Override
+    protected BaseService<Long, CreateVisitorDTO, UpdateVisitorDTO, ResponseVisitorDTO> getService() {
+        return visitorService;
+    }
+
+    @Override
+    public Class<Visitor> getEntityClass() {
+        return Visitor.class;
+    }
+}
