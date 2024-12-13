e8477891dbe901fcb89eda0e8d9e5b392302f8c6
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:41:56 +0100

    add custom exception for Resource Not Found

--- src/main/java/org/wrmList/waitingList/shared/exception/ResourceNotFoundException.java
+++ src/main/java/org/wrmList/waitingList/shared/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package org.wrmList.waitingList.shared.exception;
 import org.springframework.http.HttpStatus;
 import static org.wrmList.waitingList.shared.constant.ErrorCodes.RESOURCE_NOT_FOUND;
 
+// Resource Not Found
 public class ResourceNotFoundException extends BaseException {
 
     public ResourceNotFoundException(String message) {
