32ea8deafcabd01063cde37b7ea6af42b1545592
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:37:38 +0100

    upgrade api to Version 2

--- src/main/java/org/wrmList/waitingList/shared/constant/ApiConstants.java
+++ src/main/java/org/wrmList/waitingList/shared/constant/ApiConstants.java
@@ -1,7 +1,7 @@
 package org.wrmList.waitingList.shared.constant;
 
 public final class ApiConstants {
-    public static final String API_VERSION = "v1";
+    public static final String API_VERSION = "v2";
     public static final String API_BASE_PATH = "/api/" + API_VERSION;
     
     private ApiConstants() {}
