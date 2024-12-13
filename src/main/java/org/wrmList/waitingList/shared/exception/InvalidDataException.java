d9f5ac3a60a1e6abb14c07e1a4b9d63d7953ba61
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:40:47 +0100

    add custom exception for Invalid data

--- src/main/java/org/wrmList/waitingList/shared/exception/InvalidDataException.java
+++ src/main/java/org/wrmList/waitingList/shared/exception/InvalidDataException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 
 import static org.wrmList.waitingList.shared.constant.ErrorCodes.INVALID_DATA;
 
+// Invalid Data Exception
 public class InvalidDataException extends BaseException {
     public InvalidDataException(String message) {
         super(message, INVALID_DATA, HttpStatus.BAD_REQUEST);
