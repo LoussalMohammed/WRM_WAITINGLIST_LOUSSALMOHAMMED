ef847285845843e845e7b580514ec7fe21a39b81
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:50:33 +0100

    add uniqueness validator

--- src/main/java/org/wrmList/waitingList/util/validation/UniquenessValidator.java
+++ src/main/java/org/wrmList/waitingList/util/validation/UniquenessValidator.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+// Add Uniqueness Validator
+
 @Slf4j
 @Component
 public class UniquenessValidator {
