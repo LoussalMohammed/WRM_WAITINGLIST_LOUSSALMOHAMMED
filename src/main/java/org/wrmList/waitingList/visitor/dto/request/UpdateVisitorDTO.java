b053298c04f6f4cd4a399acec90aac9a86e825a6
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:53:02 +0100

    add update visitor dto record

--- src/main/java/org/wrmList/waitingList/visitor/dto/request/UpdateVisitorDTO.java
+++ src/main/java/org/wrmList/waitingList/visitor/dto/request/UpdateVisitorDTO.java
@@ -2,6 +2,8 @@ package org.wrmList.waitingList.visitor.dto.request;
 
 import jakarta.validation.constraints.Size;
 
+// Update Visitor DTO
+
 public record UpdateVisitorDTO(
         @Size(max = 15, min = 2)
         String name
