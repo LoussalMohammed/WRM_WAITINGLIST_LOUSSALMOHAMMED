c5eab51c47efca51d376077e220267fe8922d980
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:51:34 +0100

    add waiting list main file

--- src/main/java/org/wrmList/waitingList/WaitingListApplication.java
+++ src/main/java/org/wrmList/waitingList/WaitingListApplication.java
@@ -3,6 +3,8 @@ package org.wrmList.waitingList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Waiting List Application
+
 @SpringBootApplication
 public class WaitingListApplication {
 
