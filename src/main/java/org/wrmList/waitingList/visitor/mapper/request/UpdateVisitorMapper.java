617e324d1ffc7e92f77e829996dbff0f14c11db2
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:59:51 +0100

    add update visitor mapper interface

--- src/main/java/org/wrmList/waitingList/visitor/mapper/request/UpdateVisitorMapper.java
+++ src/main/java/org/wrmList/waitingList/visitor/mapper/request/UpdateVisitorMapper.java
@@ -1,9 +1,11 @@
-package org.wrmList.waitingList.visitor.mapper.response;
+package org.wrmList.waitingList.visitor.mapper.request;
 
 import org.mapstruct.Mapper;
 import org.wrmList.waitingList.common.mapper.BaseMapper;
 import org.wrmList.waitingList.visitor.dto.request.UpdateVisitorDTO;
 import org.wrmList.waitingList.visitor.entity.Visitor;
+
+// Update Visitor Mapper
 
 @Mapper(componentModel = "spring")
 public interface UpdateVisitorMapper extends BaseMapper<Visitor, UpdateVisitorDTO> {
--- src/main/java/org/wrmList/waitingList/visitor/mapper/response/EmbeddableVisitorMapper.java
+++ src/main/java/org/wrmList/waitingList/visitor/mapper/response/EmbeddableVisitorMapper.java
@@ -1,9 +1,11 @@
-package org.wrmList.waitingList.visitor.mapper.request;
+package org.wrmList.waitingList.visitor.mapper.response;
 
 import org.mapstruct.Mapper;
 import org.wrmList.waitingList.common.mapper.BaseMapper;
 import org.wrmList.waitingList.visitor.dto.response.EmbeddableVisitorDTO;
 import org.wrmList.waitingList.visitor.entity.Visitor;
+
+// Embeddable Visitor Mapper
 
 @Mapper(componentModel = "spring")
 public interface EmbeddableVisitorMapper extends BaseMapper<Visitor, EmbeddableVisitorDTO> {
