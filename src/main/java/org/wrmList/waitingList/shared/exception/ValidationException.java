084b5ae95b99f95752d2cd68afefd70e1f119d7d
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:42:23 +0100

    add custom exception for Validation Exception

--- src/main/java/org/wrmList/waitingList/shared/exception/ValidationException.java
+++ src/main/java/org/wrmList/waitingList/shared/exception/ValidationException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 
 import static org.wrmList.waitingList.shared.constant.ErrorCodes.VALIDATION_ERROR;
 
+// Validation Exception
 public class ValidationException extends BaseException {
     public ValidationException(String message) {
         super(message, VALIDATION_ERROR, HttpStatus.BAD_REQUEST);
