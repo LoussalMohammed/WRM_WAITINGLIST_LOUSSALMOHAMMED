695b7bc2f6d05b84a6f161c69529c39e22969d51
loussalmohammed <hplaptopsimo@gmail.com>
2024-12-13 09:53:30 +0100

    add embeddable visitor dto record

--- src/main/java/org/wrmList/waitingList/visitor/dto/response/EmbeddableVisitorDTO.java
+++ src/main/java/org/wrmList/waitingList/visitor/dto/response/EmbeddableVisitorDTO.java
@@ -3,6 +3,8 @@ package org.wrmList.waitingList.visitor.dto.response;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+// Embeddable Visitor DTO
+
 public record EmbeddableVisitorDTO(
         @Size(max = 15, min = 2)
         @NotBlank
